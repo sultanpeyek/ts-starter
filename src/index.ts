7fd1ee8d00e7822cb9a73ce715e7e6eb6f4c7870
SULTAN PEYEK <sultanpeyek999@gmail.com>
2023-05-10 16:55:33 +0700

    Initialize repo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+void (() => {
+  try {
+    console.log("Hello world!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
